22b3236016a682bee6fee6eddfc5d2202f6fdf80
spustoff <vyacheslavpustov05@gmail.com>
2023-12-08 15:07:37 +0300

    Initial Commit

---
+++ App273/App273App.swift
@@ -0,0 +1,17 @@
+//
+//  App273App.swift
+//  App273
+//
+//  Created by Вячеслав on 12/8/23.
+//
+
+import SwiftUI
+
+@main
+struct App273App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
